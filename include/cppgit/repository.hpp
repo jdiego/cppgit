fad38b757a0afb892af2237fe280370d6a123f4f
Diego Saraiva <diegosaraiva@gmail.com>
2022-11-14 13:59:47 -0300

    adding some functions to repository

--- include/cppgit/repository.hpp
+++ include/cppgit/repository.hpp
@@ -2,9 +2,21 @@
 #define CPPGIT_HPP
 
 #include "cppgit/git2api.hpp"
-#include <tuple>
+#include <git2/types.h>
+#include <memory>
+#include <string>
+#include <optional>
+
 
 namespace cppgit {
+    
+    enum class branch_type
+    {
+        LOCAL,
+        REMOTE,
+        ALL
+    };
+
 // =====================================================================================
 //        Class:  Repository
 //  Description:
@@ -13,8 +25,12 @@ namespace cppgit {
 class Repository: private Git2API {
 public:
     // ====================  LIFECYCLE     =======================================
-    Repository();
+    Repository(std::string_view path, bool is_bare);
     // ====================  ACCESSORS     =======================================
+    [[nodiscard]] bool is_bare() const;
+    [[nodiscard]] const char* path() const;
+    [[nodiscard]] const char* workdir() const;
+    [[nodiscard]] static std::optional<std::string> discover_path(std::string_view start_path, bool across_fs = false, std::string_view ceiling_dirs = "");
     // ====================  MUTATORS      =======================================
 
     // ====================  OPERATORS     =======================================
@@ -28,7 +44,8 @@ private:
     // ====================  METHODS       =======================================
 
     // ====================  DATA MEMBERS  =======================================
-
+    struct Destroy { void operator() (git_repository *) const; };
+    std::unique_ptr<git_repository, Destroy> m_repo;
 }; // -----  end of class Repository  -----
 } // namespace cppgit
 
--- src/repository.cpp
+++ src/repository.cpp
@@ -1,8 +1,39 @@
 #include "cppgit/repository.hpp"
+#include "cppgit/exception.hpp"
 #include <git2.h>
 
 namespace cppgit {
-    Repository::Repository(){
-        
-    } 
+    Repository::Repository(std::string_view path, bool is_bare){
+        git_repository* repo = nullptr;
+        if (git_repository_init(&repo, path.data(), static_cast<unsigned int>(is_bare)) != 0) {
+            throw Exception();
+        }
+        m_repo.reset(repo);
+    }
+    
+    void Repository::Destroy::operator() (git_repository* repo) const
+    {
+        git_repository_free(repo);
+    }
+
+    bool Repository::is_bare() const {
+        return git_repository_is_bare(m_repo.get()) != 0;
+    }
+    
+    const char* Repository::path() const
+    {
+        return git_repository_path(m_repo.get());
+    }
+    const char* Repository::workdir() const
+    {
+        return git_repository_workdir(m_repo.get());
+    }
+    
+    std::optional<std::string> Repository::discover_path(std::string_view start_path, bool across_fs, std::string_view ceiling_dirs) {
+        git_buf buffer = {.ptr = nullptr, .size = 0};
+        if (git_repository_discover(&buffer, start_path.data(), static_cast<int>(across_fs), ceiling_dirs.data()) != 0) {
+            return std::nullopt;
+        }
+        return std::string(buffer.ptr, buffer.size);
+    }
 }
